58f66673063e53c3344d53e92bdb03610b68f17a
hariasahil25 <hariyasahil1025@gmail.com>
2026-05-02 19:38:08 +0530

    Add demonstration of Python data types
    This program demonstrates various data types in Python, including integer, float, string, boolean, list, tuple, dictionary, and set, along with their respective values and types.

---
+++ data_types.py
@@ -0,0 +1,39 @@
+# Program to demonstrate different data types in Python
+
+# Integer
+num = 25
+
+# Float
+price = 99.99
+
+# String
+name = "Ash King"
+
+# Boolean
+is_student = True
+
+# List
+subjects = ["Python", "Java", "C++"]
+
+# Tuple
+coordinates = (10, 20)
+
+# Dictionary
+student = {
+    "name": "Ash King",
+    "age": 20,
+    "course": "BCA"
+}
+
+# Set
+unique_numbers = {1, 2, 3, 4, 5}
+
+# Printing values and their data types
+print("Value:", num, "| Data Type:", type(num))
+print("Value:", price, "| Data Type:", type(price))
+print("Value:", name, "| Data Type:", type(name))
+print("Value:", is_student, "| Data Type:", type(is_student))
+print("Value:", subjects, "| Data Type:", type(subjects))
+print("Value:", coordinates, "| Data Type:", type(coordinates))
+print("Value:", student, "| Data Type:", type(student))
+print("Value:", unique_numbers, "| Data Type:", type(unique_numbers))
